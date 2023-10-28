cb253006df60907025c0f4203fbb16b16d6a59c5
Parth Sundarka <77690791+parthusun8@users.noreply.github.com>
2023-10-28 17:23:06 +0530

    Project Complete

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import face_recognition
 import cvzone
 import numpy as np
 from deepface import DeepFace
+import gspread
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -30,10 +31,36 @@ encodeListKnown, studentIds = encodeListKnownWithIds
 # print(studentIds)
 print("Encode File Loaded")
 
+sa = gspread.service_account_from_dict({
+    "type": "service_account",
+    "project_id": "srmasv-grader",
+    "private_key_id": "ad28c202755298e854d1a6ecfd6f1806ba60653c",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCrcWpdhASvnH4s\nBoEHNN+Dg2uaEZT2u2fkInZ6/SaA08rQskxl+0c30lmWw9ZkGbF9gOXBFTpuLu7T\n00TKEQ14G4dXz+d+4Eig2Krtgj8ZvUF5SbQUYaOnhSeje/fxp/V/DPa7/GgcjOkk\nhg+kxKlZe1p29Hvza0Vkuhu6rGtRmcn6SzpF5AQf5+/UFl0PyOeviUNsQ31RG9Ab\nVIRuPjCB8/CfJcGaB+v4mpTzb49yvX+or2cz/D+oJq9En/OaST3BRSrfpuqFGiv8\nAsaw1lEikov1bf9G534chslbFw1suPmm/TfOcrra6hrta7n+Vth2/eogxI7FuJwy\nmBM7oFVVAgMBAAECggEABy29EURdY925ehjISf0gnM5McPY/1rhQfdYkQDsduR2P\n1lRDlwOXXv2BAICbwgjstyobX2MxqqUZcqeGhQWDcF41Y8ESBZcuQ1np3P8Wqvpo\nkEO8oBvZ2+0F6gu0p/BApdIXpMB0EDn66yttN+4qKdPcnQguBn/H4eivD453rJd3\nzgqk8g10MgjqaxyYpC0DhDHAMwsYyLoIx6b/js2VRnyHGFs2DT89d6QXQgkbWa7Q\nnmZLhFxesvfFjUUSm+cKgcstAEZif1/pK00/vougvny2pQPVFJpXcJjwgCPLGLGJ\nRaIkFBiSpkKwjUrBZgIGwWF/9p8iswfGVDFCxsfS0wKBgQDnHtgDGhUwJbaV+yek\nBHzT0f7UQ6pOJ/SocGfUovAOkxdgb6MxM1L5n+nUzU3jJc7twy/rSaZGx3vfrAWt\naFZFs1bfM7M8qDjQaScAmE2DRKTLFCNufpgiwGwj4GvVeCuphgHQq0HCIqZHRuNx\nHzuKlZx4bf6gim40adpOYnBQnwKBgQC95f8WdH1BRCUWfYYPb22gyjCDg8atuTUx\nvbJAtKtmAwfDjUiIDkXdyZB/ZQ1I19rHtD/iaOCIE1NlwY9yzhXCMhAttwxgER1y\neMa/mbBoyxad//RPqEnBbGTtHhlS7rmBBwZYNGeNv6ZXeCOfsuUQNainUACQUioU\nE3kIlnYRiwKBgQCcxDIXIIkAqIQJhVslCJo2/ziPd/o7myR7YAs9kuurpx/zHKYf\nyEVe5pYv7rYG/e03Hu8Q9FrhpYujcsZDEgN6saOaMDJCUYR/8OMwpx3kjRoOWXGT\nEDFXWRfA2gequyE6kpgGlzM6YFBTBoVdhKbZYJPKqClPcbZAvRADnQe71wKBgEFv\nmqnrNCokSD9qu6Jf/D/WzEbVRsYgNvNP8beYSiOZ0bgR3Dd965dUWKV9dclvECWW\nuBjVmOFq/2bl/v9JgnfrdmW6WNpVq3cBpULqu74wlTeWtmIolFnBdzm6EHHibyvF\n9uq4DCLtMm2bqXLjW0ltpBMbX0Zb+cH3P9K0vjSnAoGBAKWq8JSTPTuvx6YQaa1j\nUoBToQrzx2usIZL9mvMbjMVL4E+Li+SoHaCFXEFCDllQGaNVFn1bFadHcBqut+64\ntzz+PhrTwDItwZKf5dSiz8JjE5HTLTX8cISfKVVGONimLRkk2ujRbVTk8jmoVrWa\nfwa7IL2PQOs6Gow3yvRq9Lpl\n-----END PRIVATE KEY-----\n",
+    "client_email": "serviceaccount@srmasv-grader.iam.gserviceaccount.com",
+    "client_id": "109484706480290888675",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/serviceaccount%40srmasv-grader.iam.gserviceaccount.com"
+  })
+
+sh = sa.open("EMPLOYEE EMOTION RECORDS")
+
+wks = sh.worksheet("Sheet1")
+
+records = wks.get_all_records()
+studentInfos = {}
+for index, i in enumerate(records):
+    print(i)
+    studentInfos[i['Reg No']] = i
+    studentInfos[i['Reg No']]['index'] = str(index + 2)
+
+
 modeType = 0
 counter = 0
 id = -1
 imgStudent = []
+
 studentInfo = {
     "total_attendance": "total",
     "major": "major",
@@ -74,10 +101,6 @@ while True:
                 id = studentIds[matchIndex]
                 if counter == 0:
                     cvzone.putTextRect(imgBackground, "Loading", (275, 400))
-                    imgStudent = cv2.imread("Images/" + studentIds[matchIndex] + ".png")
-                    imgStudent = cv2.resize(imgStudent, (216, 216))
-                    result = DeepFace.analyze(img, actions=['emotion'], enforce_detection=False)
-                    print(result[0]['dominant_emotion'])
                     cv2.imshow("Face Attendance", imgBackground)
                     cv2.waitKey(1)
                     counter = 1
@@ -116,25 +139,33 @@ while True:
                 #     modeType = 2
 
                 imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+                result = DeepFace.analyze(img, actions=['emotion'], enforce_detection=False)
+                print(result[0]['dominant_emotion'])
+                studentInfos[id]["DAY 4"] = result[0]['dominant_emotion']
+                print(f"I{studentInfos[id]['index']}")
+                wks.update("I" + studentInfos[id]['index'], studentInfos[id]["DAY 4"])
 
                 if True:
                     # cv2.putText(imgBackground, str(studentInfo['total_attendance']), (861, 125),
                     #             cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
-                    cv2.putText(imgBackground, str(studentInfo['major']), (1006, 550),
+                    cv2.putText(imgBackground, str(studentInfos[id]['Major']), (1006, 550),
                                 cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
                     cv2.putText(imgBackground, str(id), (1006, 493),
-                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
-                    cv2.putText(imgBackground, str(studentInfo['standing']), (910, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.4, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(studentInfos[id]['DAY 4']), (910, 625),
                                 cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
-                    cv2.putText(imgBackground, str(studentInfo['year']), (1025, 625),
+                    cv2.putText(imgBackground, str(studentInfos[id]['Passing_Year']), (1025, 625),
                                 cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
-                    cv2.putText(imgBackground, str(studentInfo['starting_year']), (1125, 625),
+                    cv2.putText(imgBackground, str(studentInfos[id]['Year']), (1125, 625),
                                 cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
 
-                    (w, h), _ = cv2.getTextSize(studentInfo['name'], cv2.FONT_HERSHEY_COMPLEX, 1, 1)
+                    (w, h), _ = cv2.getTextSize(studentInfos[id]['Name'], cv2.FONT_HERSHEY_COMPLEX, 1, 1)
                     offset = (414 - w) // 2
-                    cv2.putText(imgBackground, str(studentInfo['name']), (808 + offset, 445),
+                    cv2.putText(imgBackground, str(studentInfos[id]['Name']), (808 + offset, 445),
                                 cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 50), 1)
+
+                    imgStudent = cv2.imread("Images/" + studentIds[matchIndex] + ".png")
+                    imgStudent = cv2.resize(imgStudent, (216, 216))
 
                     imgBackground[175:175 + 216, 909:909 + 216] = imgStudent
 
